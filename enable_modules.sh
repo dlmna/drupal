65dcf3f71c4e8769adcb736ae1d86631be2d08ad
Daniel Mania <daniel.mania@klarblick.org>
2016-08-16 13:48:18 +0200

    add module installation script

---
+++ enable_modules.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+drush -y en config_translation
+
+drush -y en features
+drush -y en features_ui
+
+drush -y en appff_bundles
+drush -y en appff_languages
+drush -y en appff_core
+drush -y en appff_site
+drush -y en appff_user
+drush -y en appff_language
+
+drush -y en appff_accomodation
+drush -y en appff_advisory_place
+drush -y en appff_article
+drush -y en appff_topic
+drush -y en appff_menu
+
+
